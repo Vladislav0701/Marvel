27af1c1b7e0aac5a2f17ac9ec84af5583076506a
Vladislav <des.kzv1318@gmail.com>
2022-01-06 10:48:26 +0300

    Added navigation between pages. Moved pages into separate components.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,45 +1,28 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import ComicsList from "../comicsList/ComicsList";
+import { MainPage, ComicsPage } from "../pages";
 import SingleComic from "../singleComic/SingleComic";
-
-import decoration from '../../resources/img/vision.png';
-
-
 
 const App = () => {
 
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
-
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                {/* <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
-                <ComicsList/>
-                {/* <SingleComic id={95768}/> */}
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <MainPage/>
+                        </Route>
+                        <Route exact path="/comics">
+                            <ComicsPage/>
+                        </Route>
+                        {/* <SingleComic id={95768}/> */}
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     )
 }
 
